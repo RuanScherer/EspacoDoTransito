bbdd59b5bbde4b0f288cf67ee8b80b85def2e156
Ruan Vitor Scherer <ruan.vscherer@gmail.com>
2020-01-24 13:16:44 -0300

    New message and search messages methods created

---
+++ Controller/MessageController.php
@@ -0,0 +1,47 @@
+<?php 
+
+include_once("./Model/Message.php");
+
+class MessageController extends Message
+{
+
+	public function __construct($post)
+	{
+		parent::__construct($post);
+	}
+
+	// Create a new message
+	public function new()
+	{
+		$query = "insert into tb_message(email, name, topic, message, date) values('".$this->email."', '".$this->name."', '".$this->topic."', '".$this->message."', date(now()))";
+		$response = mysqli_query($this->connect(), $query);
+
+		if($response)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	// Search for all messages
+	public function getAll()
+	{
+		$query = "select * from tb_message";
+		$response = mysqli_query($this->connect(), $query);
+
+		if($response)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+}
+
+?>
