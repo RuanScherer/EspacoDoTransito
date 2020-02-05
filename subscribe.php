c440fa81d942802cc36317f8222d3a12f2a72212
Ruan Vitor Scherer <ruan.vscherer@gmail.com>
2020-02-05 20:07:29 -0300

    Tip for renach

--- subscribe.php
+++ subscribe.php
@@ -22,12 +22,31 @@ if(isset($_GET['send']))
     <link rel="stylesheet" href="bootstrap/bootstrap.css">
     <link rel="stylesheet" href="styles.css">
 
+    <style type="text/css">
+    	.hidden {
+    		display: none !important;
+    	}
+    	#tip-background {
+    		position: fixed;
+    		width: 100vw;
+    		height: 100vh;
+    		background-color: rgba(0, 0, 0, 0.7);
+    	}
+    	#renach-tip {
+    		max-width: 95%;
+    	}
+    </style>
+
     <!-- Icon -->
     <link rel="sortcut icon" href="assets/logo.png" type="image/png"/>
 
     <title>Espaço do Trânsito - Inscrever-se</title>
   </head>
   <body>
+  	<div class="hidden p-3 d-flex flex-column justify-content-center align-items-center" id="tip-background">
+  		<img src="assets/renach.jpeg" id="renach-tip">
+  		<button class="btn btn-sm btn-secondary mt-3" id="close-tip">Entendi</button>
+  	</div>
 		<!-- HIGHLIGHT -->
 		<main class="d-flex flex-column align-items-center w-100 text-center text-light pb-5 background-right">
 			<!-- HEADER -->
@@ -128,7 +147,7 @@ if(isset($_GET['send']))
 			  <div class="form-group">
 			    <label for="renach">RENACH</label>
 			    <input type="text" class="form-control field" id="renach" name="renach" required>
-			    <small class="text-muted" id="renach">Não sabe oque é o RENACH? Clique aqui.</small>
+			    <a class="text-muted btn-link btn p-0" id="renach-help">Não sabe oque é o RENACH? Clique aqui.</a>
 			  </div>
 			  <div class="form-group">
 			    <label for="schooling">Escolaridade</label>
@@ -198,6 +217,16 @@ if(isset($_GET['send']))
     	phone = document.querySelector("#phone");
     	cpf = document.querySelector("#cpf");
     	rg = document.querySelector("#rg");
+    	renach = document.querySelector("#renach-help");
+    	tip = document.querySelector("#tip-background");
+
+    	renach.addEventListener('click', () => {
+    		tip.classList.remove("hidden");
+    	});
+
+    	document.querySelector("#close-tip").addEventListener('click', () => {
+    		tip.classList.add("hidden")
+    	});
 
     	fields = [...document.getElementsByClassName("field")];
 
